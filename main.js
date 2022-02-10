1af38e0f276963afe8af9d1063de629c77bb1985
RatushnyiArtem <92036528+RatushnyiArtem@users.noreply.github.com>
2022-02-10 19:40:58 +0100

    mathematic

---
+++ main.js
@@ -0,0 +1,146 @@
+// FOR 1
+// let k = +prompt('Ввести число K')
+// let n = prompt('Сколько раз будем выводить')
+// for(let a = 1; a<=n ; a++){
+//     console.log(k)
+// }
+// FOR 2
+// let a = +prompt('Введите первое число')
+// let b = +prompt('Введите второе число')
+// let n = (b-a)+1
+// if (a<b){
+//     for(let i = a; i<=b; i++){
+//         console.log(i)
+//     }
+//     console.log(`Количество цифр между ними = ${n}`)
+// }
+//  else{
+//     alert('Ошибка')
+// }
+// FOR 3
+// let a = +prompt('Введите первое число')
+// let b = +prompt('Введите второе число')
+// let n = (b-a)-1
+// if (a<b){
+//     for(let i = a+1; i<b; i++){
+//         console.log(i)
+//     }
+//     console.log(`Количество цифр между ними = ${n}`)
+// }
+//  else{
+//     alert('Ошибка')
+// }
+// FOR 4
+// let c = +prompt('Введите цену за 1кг')
+// for(let a = 1; a<=10; a++){
+//     console.log(`Цена за ${a} кг = ${c*a} грн`)
+//     console.log('-------------------------')
+// }
+// FOR 5
+// let c = +prompt('Введите цену за 1кг')
+// for(let a = 0.1; a<=1; a+=0.1){
+//     let s = c*a
+//     console.log(`Цена за ${a.toFixed(1)} кг = ${Math.round(s)} грн`)
+//     console.log('-------------------------')
+// }
+// FOR 6
+// let c = +prompt('Введите цену за 1кг')
+// for(let a = 1.2; a<=2; a+=0.2){
+//     let s = c*a
+//     console.log(`Цена за ${a.toFixed(1)} кг = ${Math.round(s)} грн`)
+//     console.log('-------------------------')
+// }
+// FOR 7
+// let a = +prompt('Введите первое число')
+// let b = +prompt('Введите второе число')
+// k=0
+// if (a<b){
+//     for(let i = a; i<=b; i++){
+//         k+=i
+        
+        
+//     } console.log(k)
+// }
+//  else{
+//     alert('Ошибка')
+// }
+// FOR 8 
+// let a = +prompt('Введите первое число')
+// let b = +prompt('Введите второе число')
+// k=1
+// if (a<b){
+//     for(let i = a; i<=b; i++){
+//         k*=i
+        
+        
+//     } console.log(k)
+// }
+//  else{
+//     alert('Ошибка')
+// }
+// FOR 9 
+// let a = +prompt('Введите первое число')
+// let b = +prompt('Введите второе число')
+// k=0
+// if (a<b){
+//     for(let i = a; i<=b; i++){
+//         k+=i**2
+        
+        
+//     } console.log(k)
+// }
+//  else{
+//     alert('Ошибка')
+// }
+// FOR 10
+// let n = +prompt('Введите число')
+// let k = 0
+// for (let a = 1; a<=n; a++){
+//     k+=(1/a)
+// } console.log(`Суммой этого выражения является = ${k}`)
+// FOR 11 
+// let n = +prompt('Введите число')
+// let k = 0
+// for (let a = 0; a<=n; a++){
+//     k+=(n+a)**2
+// } console.log(`Суммой этого выражения является = ${k}`)
+// FOR 12
+// let n = +prompt('Введите число')
+// c = 1
+// let k = 1
+// for (let a = 1.1; c<=n; a+=0.1){
+//     k*=a
+//     c++
+    
+// } console.log(`Произвидением этого выражения является = ${k.toFixed(1)}`)
+// FOR 13
+// let n = +prompt('Введите число')
+// c = 1
+// let k = 0
+// for (let a = 1.1; c<=n; a+=0.2){
+//     k+= a - (a + 0.1)
+//     c++
+    
+// } console.log(`Суммой этого выражения является = ${k.toFixed(1)}`)
+// FOR 14
+// let n = +prompt('Введите число')
+// let c = (2*n)-1
+// let k = 0
+// for(let a = 1; a<=c ; a+=2){
+//    k+=a
+// }console.log('Сумма равна = ' + k)
+// FOR 15
+// let a = +prompt('Введите число')
+// let n = +prompt('Введите степень')
+// for(n;n<0;n=n){
+// } console.log('A в степени N = ' + a**n)
+// FOR 16
+// let a = +prompt('Введите число')
+// let n = +prompt('Введите степень')
+// k = 1
+// for(i=1;i<=n;i++){
+//     k = a**i
+//     console.log('A в степени N = ' + k)
+//     console.log('-------------------------')
+// }
+
